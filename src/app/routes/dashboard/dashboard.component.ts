16e8c5f9aab867b186ee39ac8c5bc924381b8ee0
朱志军 <2285561715@qq.com>
2019-09-01 19:41:00 +0800

    我的提交

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { _HttpClient } from '@delon/theme';
+import { Component, OnInit, Input, TemplateRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { _HttpClient, ModalHelper, SettingsService } from '@delon/theme';
+import { STColumn, STComponent } from '@delon/abc';
+import { SFSchema } from '@delon/form';
+import { NzFormatEmitEvent, NzDrawerRef, NzDrawerService, NzMessageService, NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+  constructor(
+    private http: _HttpClient,
+    private cdr: ChangeDetectorRef,
+    private modal: ModalHelper,
+    private drawerService: NzDrawerService,
+    private msgSrv: NzMessageService,
+    private modalService: NzModalService,
+    // private settingService: SettingsService,
+    public settings: SettingsService,
+  ) {}
 
-  constructor(private http: _HttpClient) { }
+  listOfData: any[] = [];
+  value: string;
+  visible = false;
 
   ngOnInit() {
+    this.loadInfo();
+    console.log('dfd=');
+    console.log(this.settings.user.userFrom);
   }
 
+  loadInfo(): void {
+    this.http.get('/api/adapply').subscribe((res: any[]) => {
+      this.listOfData = res;
+      this.cdr.detectChanges();
+    });
+  }
+  //
 }
