e28604abe6f60293f647432737dc84b2c69dcff4
朱志军 <2285561715@qq.com>
2019-10-22 16:55:58 +0800

    我的提交

--- src/app/layout/passport/passport.component.ts
+++ src/app/layout/passport/passport.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
+import { _HttpClient } from '@delon/theme';
 
 @Component({
   selector: 'layout-passport',
@@ -6,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./passport.component.less'],
 })
 export class LayoutPassportComponent {
+  constructor(private msgSrv: NzMessageService, public http: _HttpClient) {}
   links = [
     {
       title: '帮助',
@@ -20,4 +23,8 @@ export class LayoutPassportComponent {
       href: '',
     },
   ];
+
+  shoucang(sTitle: string, sURL: string): void {
+    this.msgSrv.success('加入收藏失败，请使用Ctrl+D进行添加!');
+  }
 }
